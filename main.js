1f4e811ab06f4861c3ac1cf6cd599c50fb78439d
Benne2000 <benedikt.stockert00@gmail.com>
2025-08-12 09:16:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -49,6 +49,9 @@
 
     connectedCallback() {
       if (!window.L) {
+        this._shadowRoot.host.style.display = "block";
+        this._shadowRoot.host.style.height = "400px";
+
         const link = document.createElement('link');
         link.rel = 'stylesheet';
         link.href = 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css';
@@ -147,3 +150,4 @@
     customElements.define('geo-map-widget', GeoMapWidget);
   }
 })();
+
